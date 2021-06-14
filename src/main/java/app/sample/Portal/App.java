2575ed26741f986de63e0a1bb6d6e4617c890ae0
gajendra.saxena <gajendra.akash02@gmail.com>
2021-06-14 09:38:37 +0530

    Added POm update

--- src/main/java/app/sample/Portal/App.java
+++ src/main/java/app/sample/Portal/App.java
@@ -1,5 +1,7 @@
 package app.sample.Portal;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,8 +10,9 @@ import org.testng.annotations.Test;
 public class App {
 	public static WebDriver driver;
 
-	public static void main(String[] args) throws InterruptedException {
-
+	// public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void loadbrowser() throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver",
 				"E:\\eclipse_Workspace\\WorkspaceTesting\\latestCucumberSeleniumFramework-master\\src\\test\\resources\\drivers\\chromedriver.exe");
 
@@ -22,6 +25,9 @@ public class App {
 				.sendKeys("gajendrasaxena");
 		driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("9806480683");
 		driver.findElement(By.xpath("//button[@class='btn-primary loginButton']")).click();
+		Thread.sleep(5000);
 		driver.quit();
+		Runtime runtime = Runtime.getRuntime();
+		runtime.exec("taskkill /im chromedriver.exe /f");
 	}
 }
