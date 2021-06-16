60d8b264e6a49ce0ce71cc23f469d9c4225f4941
gajendra.saxena <gajendra.akash02@gmail.com>
2021-06-16 09:59:22 +0530

    Added NEw Url

--- src/test/java/app/sample/Portal/NewSample.java
+++ src/test/java/app/sample/Portal/NewSample.java
@@ -1,11 +1,19 @@
 package app.sample.Portal;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewSample {
-
+	public static WebDriver driver;
 	@Test
 	public void applaunch() {
+		System.setProperty("webdriver.chrome.driver",
+				"E:\\eclipse_Workspace\\WorkspaceTesting\\latestCucumberSeleniumFramework-master\\src\\test\\resources\\drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.naukri.com/");
 		System.out.println("Test");
 	}
 
